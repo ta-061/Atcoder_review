fc30eece91540b5d340424bc901f00e5399b42e6
ta-061 <fjttrj67xm@i.softbank.jp>
2024-05-29 05:49:19 +0900

    chore: Update C++ standard library includes

---
+++ tmp.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() { 
+    char c = 'a';
+    bool b=true;
+    int a=1;
+    string s="Hello, World!";
+    string str = "Hello, Worldaaaaaaaaaaaaaaaaaaaaaaaaaaaa!";
+    cout << "Length of string: " << str.length() << endl; // 文字列の長さを表示
+    cout << "Size of string object: " << sizeof(str) << " bytes" << endl; // stringオブジェクトのサイズを表示
+    cout << str+s << endl;
+    return 0;
+}

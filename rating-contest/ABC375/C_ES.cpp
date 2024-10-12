699c5d1fdb0b79c13b02b7d725fcc1838a8411f9
ta-061 <fjttrj67xm@i.softbank.jp>
2024-10-12 23:04:37 +0900

    全単射

---
+++ rating-contest/ABC375/C_ES.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+
+template<>
+struct std::vector<bool>: std::basic_string<bool> {
+    using std::basic_string<bool>::basic_string, std::basic_string<bool>::operator =;
+    explicit vector(size_t n): vector(n, false) {}
+};
+
+//ラムダ記法
+/*auto func = lambda($(0) * 3 + 1);*/
+// 引数を取得するマクロ
+#define $(idx) (std::get<(idx)>(std::forward_as_tuple(_args...)))
+using namespace std;
+
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+
+template<>
+struct std::vector<bool>: std::basic_string<bool> {
+    using std::basic_string<bool>::basic_string, std::basic_string<bool>::operator =;
+    explicit vector(size_t n): vector(n, false) {}
+};
+
+//ラムダ記法
+/*auto func = lambda($(0) * 3 + 1);*/
+// 引数を取得するマクロ
+#define $(idx) (std::get<(idx)>(std::forward_as_tuple(_args...)))
+using namespace std;
+
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+
+template<>
+struct std::vector<bool>: std::basic_string<bool> {
+    using std::basic_string<bool>::basic_string, std::basic_string<bool>::operator =;
+    explicit vector(size_t n): vector(n, false) {}
+};
+
+//ラムダ記法
+/*auto func = lambda($(0) * 3 + 1);*/
+// 引数を取得するマクロ
+#define $(idx) (std::get<(idx)>(std::forward_as_tuple(_args...)))
+using namespace std;
+
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+
+int main() {
+    int N = 8; // 入力は固定で8x8
+    vector<string> A(N);
+    
+    // 入力を受け取る
+    for (int i = 0; i < N; ++i) {
+        cin >> A[i];
+    }
+
+    // 出力用の新しい配列Bを初期化
+    vector<string> B(N, string(N, '.'));
+
+    // 入力に基づいて新しいパターンを構築
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            if (A[i][j] == '#') {
+                // 対称的に#を配置する
+                B[j][N - 1 - i] = '#'; // 90度時計回りに配置
+            }
+        }
+    }
+
+    // 変換された行列を出力
+    for (int i = 0; i < N; ++i) {
+        cout << B[i] << endl;
+    }
+
+    return 0;
+}

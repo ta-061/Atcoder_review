d2f9d412b64f27c8ee8dda4d1c92d0944afa6072
ta-061 <fjttrj67xm@i.softbank.jp>
2024-10-19 23:00:30 +0900

    DFS

---
+++ rating-contest/ABC376/D_.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+
+template<>
+struct std::vector<bool>: std::basic_string<bool> {
+    using std::basic_string<bool>::basic_string, std::basic_string<bool>::operator =;
+    explicit vector(size_t n): vector(n, false) {}
+};
+
+//ラムダ記法
+/*auto func = lambda($(0) * 3 + 1);*/
+// 引数を取得するマクロ
+#define $(idx) (std::get<(idx)>(std::forward_as_tuple(_args...)))
+// ラムダ式を短く書くマクロ
+#define lambda(...) ([&](auto&&... _args) { return (__VA_ARGS__); })
+
+//多次元vector生成関数
+/*2×2×2で全要素が1のvectorを作成
+  auto a = make_vec<int>({2, 2, 2}, 1);
+  2×2×2で全要素が0のvectorを作成
+  auto b = make_vec<int>({2, 2, 2});*/
+template<class T, size_t n, size_t idx = 0>
+auto make_vec(const size_t (&d)[n], const T& init) noexcept {
+    if constexpr (idx < n) return std::vector(d[idx], make_vec<T, n, idx + 1>(d, init));
+    else return init;
+}
+template<class T, size_t n>
+auto make_vec(const size_t (&d)[n]) noexcept {
+    return make_vec(d, T{});
+}
+
+//if (i == 3) break_with(cout << i << '\n');など
+#define exit_with(...) ({ __VA_ARGS__; exit(0); })
+#define break_with(...) ({ __VA_ARGS__; break; })
+#define continue_with(...) ({ __VA_ARGS__; continue; })
+
+vvi admap;
+
+int serch(int N){
+    int min_dist=INT_MAX;
+    vi dist(N+1);
+    for(int v : admap[1]){
+        fill(dist.begin(), dist.end(), -1);
+        queue<int> q;
+        dist[1]=0;
+        dist[v]=1;
+        q.push(v);
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(int w:admap[u]){
+                if(w==1){
+                    min_dist=min(min_dist, dist[u]+1);
+                    break;
+                }
+                if(dist[w]==-1){
+                    dist[w]=dist[u]+1;
+                    q.push(w);
+                }
+            }
+        }
+    }
+    if(min_dist == INT_MAX) return -1;
+    return min_dist;
+}
+
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    admap.resize(N+1);
+    rep(i,M){
+        int a, b;
+        cin >> a >> b;
+        admap[a].push_back(b);
+    }
+    cout << serch(N);
+}
